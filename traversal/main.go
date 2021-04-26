d7bb0e2b1ecdbf5a282e47d713acf04d7390aad4
Dipesh Dulal <nilkantha.dipesh@gmail.com>
2021-04-26 11:54:17 +0545

    feat: added ast traversal

---
+++ traversal/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"log"
+)
+
+func main() {
+	fset := token.NewFileSet()
+	node, err := parser.ParseFile(fset, "traversal/parse_this.txt", nil, parser.ParseComments)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Imports:")
+	for _, i := range node.Imports {
+		fmt.Println(i.Path.Value)
+	}
+
+	fmt.Println("Comments:")
+	for _, c := range node.Comments {
+		fmt.Println(c.Text())
+	}
+
+	fmt.Println("Functions:")
+	for _, f := range node.Decls {
+		fn, ok := f.(*ast.FuncDecl)
+		if !ok {
+			continue
+		}
+		fmt.Println("func name: ", fn.Name)
+	}
+
+	fmt.Printf("%+v\n", node)
+}
